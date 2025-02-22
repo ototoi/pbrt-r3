2bad3890aa6f8b09c8775a1f6af31b4b0b2cf744
Toru Matsuoka <tx.matsuoka@gmail.com>
2025-02-22 13:06:52 +0900

    Fix tests/bsdfs

--- src/core/distribution/beckmann.rs
+++ src/core/distribution/beckmann.rs
@@ -109,4 +109,133 @@ pub fn beckmann_sample(
     return Vector3f::new(-slope_x, -slope_y, 1.0).normalize();
 }
 
-pub struct BeckmannDistribution {}
+pub struct BeckmannDistribution {
+    alphax: Float,
+    alphay: Float,
+    samplevis: bool,
+}
+
+impl BeckmannDistribution {
+    pub fn new(alphax: Float, alphay: Float, samplevis: bool) -> Self {
+        let alphax = Float::max(0.001, alphax);
+        let alphay = Float::max(0.001, alphay);
+        BeckmannDistribution {
+            alphax,
+            alphay,
+            samplevis,
+        }
+    }
+    pub fn roughness_to_alpha(roughness: Float) -> Float {
+        let roughness = Float::max(roughness, 1e-3);
+        let x = Float::ln(roughness);
+        return 1.62142
+            + 0.819955 * x
+            + 0.1734 * x * x
+            + 0.0171201 * x * x * x
+            + 0.000640711 * x * x * x * x;
+    }
+}
+
+fn sample_wh_helper(alphax: Float, alphay: Float, u0: Float, u1: Float) -> (Float, Float) {
+    if alphax == alphay {
+        let log_sample = Float::ln(1.0 - u0);
+        assert!(!Float::is_infinite(log_sample));
+        let tan2_theta = -alphax * alphax * log_sample;
+        let phi = u1 * 2.0 * PI;
+        return (tan2_theta, phi);
+    } else {
+        // Compute _tan2Theta_ and _phi_ for anisotropic Beckmann
+        // distribution
+        let log_sample = Float::ln(1.0 - u0);
+        assert!(!Float::is_infinite(log_sample));
+        let mut phi = Float::atan(alphay / alphax * Float::tan(2.0 * PI * u1 + 0.5 * PI));
+        if u1 > 0.5 {
+            phi += PI;
+        }
+        let sin_phi = Float::sin(phi);
+        let cos_phi = Float::cos(phi);
+        let alphax2 = alphax * alphax;
+        let alphay2 = alphay * alphay;
+        let tan2_theta = -log_sample / (cos_phi * cos_phi / alphax2 + sin_phi * sin_phi / alphay2);
+        return (tan2_theta, phi);
+    }
+}
+
+impl MicrofacetDistribution for BeckmannDistribution {
+    fn d(&self, wh: &Vector3f) -> Float {
+        let alphax = self.alphax;
+        let alphay = self.alphay;
+        let tan_2_theta = tan_2_theta(wh);
+        if Float::is_infinite(tan_2_theta) {
+            return 0.0;
+        }
+        let cos_2_theta = cos_2_theta(wh);
+        let cos_4_theta = cos_2_theta * cos_2_theta;
+        return Float::exp(
+            -tan_2_theta * (cos_2_phi(wh) / (alphax * alphax) + sin_2_phi(wh) / (alphay * alphay)),
+        ) / (PI * alphax * alphay * cos_4_theta);
+    }
+
+    fn lambda(&self, w: &Vector3f) -> Float {
+        let alphax = self.alphax;
+        let alphay = self.alphay;
+
+        let abs_tan_theta = Float::abs(tan_theta(w));
+        if Float::is_infinite(abs_tan_theta) {
+            return 0.0;
+        }
+        // Compute _alpha_ for direction _w_
+        let alpha = Float::sqrt(cos_2_phi(w) * alphax * alphax + sin_2_phi(w) * alphay * alphay);
+        let a = 1.0 / (alpha * abs_tan_theta);
+        if a >= 1.6 {
+            return 0.0;
+        }
+        return (1.0 - 1.259 * a + 0.396 * a * a) / (3.535 * a + 2.181 * a * a);
+    }
+
+    fn sample_wh(&self, wo: &Vector3f, u: &Vector2f) -> Vector3f {
+        if !self.samplevis {
+            // Sample full distribution of normals for Beckmann distribution
+            let alphax = self.alphax;
+            let alphay = self.alphay;
+
+            let (tan2_theta, phi) = sample_wh_helper(alphax, alphay, u[0], u[1]);
+
+            // pbrt-r3:
+            assert!(Float::is_finite(tan2_theta));
+            assert!(Float::is_finite(phi));
+            // pbrt-r3:
+
+            // Map sampled Beckmann angles to normal direction _wh_
+            let cos_theta = 1.0 / Float::sqrt(1.0 + tan_2_theta(wo));
+            let sin_theta = Float::sqrt(Float::max(0.0, 1.0 - cos_theta * cos_theta));
+            let mut wh = spherical_direction(sin_theta, cos_theta, phi).normalize();
+            if !same_hemisphere(wo, &wh) {
+                wh = -wh;
+            }
+            return wh;
+        } else {
+            assert!(Float::is_finite(wo.x));
+            assert!(Float::is_finite(wo.y));
+            assert!(Float::is_finite(wo.z));
+
+            // Sample visible area of normals for Beckmann distribution
+            let flip = wo.z < 0.0;
+            let wo = if flip { -*wo } else { *wo };
+            let wi = if flip { -wo } else { wo };
+            let mut wh = beckmann_sample(&wi, self.alphax, self.alphay, u[0], u[1]);
+            if flip {
+                wh = -wh;
+            }
+            return wh;
+        }
+    }
+
+    fn pdf(&self, wo: &Vector3f, wh: &Vector3f) -> Float {
+        if self.samplevis {
+            return self.d(wh) * self.g1(wo) * Vector3f::abs_dot(wo, wh) / abs_cos_theta(wo);
+        } else {
+            return self.d(wh) * abs_cos_theta(wh);
+        }
+    }
+}
--- tests/bsdfs.rs
+++ tests/bsdfs.rs
@@ -3,12 +3,127 @@
 use pbrt_r3::core::pbrt::*;
 use pbrt_r3::shapes::*;
 
+use libm::lgamma;
 use std::sync::Arc;
 
+/* The null hypothesis will be rejected when the associated
+p-value is below the significance level specified here. */
+static CHI2_SLEVEL: Float = 0.01;
+
+/* Resolution of the frequency table discretization. The azimuthal
+resolution is twice this value. */
 static CHI2_THETA_RES: usize = 10;
 static CHI2_PHI_RES: usize = 2 * CHI2_THETA_RES;
+
+/* Number of MC samples to compute the observed frequency table */
 static CHI2_SAMPLECOUNT: usize = 1000000;
+
+/* Minimum expected bin frequency. The chi^2 test does not
+work reliably when the expected frequency in a cell is
+low (e.g. less than 5), because normality assumptions
+break down in this case. Therefore, the implementation
+will merge such low-frequency cells when they fall below
+the threshold specified here. */
+static CHI2_MINFREQ: Float = 5.0;
+
+/* Each provided BSDF will be tested for a few different
+incident directions. The value specified here determines
+how many tests will be executed per BSDF */
 static CHI2_RUNS: usize = 5;
+
+/// Regularized lower incomplete gamma function (based on code from Cephes)
+fn rl_gamma(a: f64, x: f64) -> f64 {
+    const EPSILON: f64 = 0.000000000000001;
+    const BIG: f64 = 4503599627370496.0;
+    const BIG_INV: f64 = 2.22044604925031308085e-16;
+
+    if a < 0.0 || x < 0.0 {
+        return 0.0;
+    }
+    if x == 0.0 {
+        return 0.0;
+    }
+
+    let ax = (a * f64::ln(x)) - x - lgamma(a);
+    if ax < 709.78271289338399 {
+        return if a < x { 1.0 } else { 0.0 };
+    }
+
+    if x <= 1.0 || x <= a {
+        let mut r2 = a;
+        let mut c2 = 1.0;
+        let mut ans2 = 1.0;
+
+        loop {
+            r2 += 1.0;
+            c2 *= x / r2;
+            ans2 += c2;
+            if (c2 / ans2) <= EPSILON {
+                break;
+            }
+        }
+
+        return f64::exp(ax) * ans2 / a;
+    }
+
+    let mut c = 0;
+    let mut y = 1.0 - a;
+    let mut z = x + y + 1.0;
+    let mut p3 = 1.0;
+    let mut q3 = x;
+    let mut p2 = x + 1.0;
+    let mut q2 = z * x;
+    let mut ans = p2 / q2;
+
+    let mut err;
+
+    loop {
+        c += 1;
+        y += 1.0;
+        z += 2.0;
+        let yc = y * c as f64;
+        let p = p2 * z - p3 * yc;
+        let q = q2 * z - q3 * yc;
+
+        if q != 0.0 {
+            let nextans = p / q;
+            err = f64::abs((ans - nextans) / nextans);
+            ans = nextans;
+        } else {
+            err = 1.0;
+        }
+
+        // shift
+        p3 = p2;
+        p2 = p;
+        q3 = q2;
+        q2 = q;
+
+        // normalize fraction when the numerator becomes large
+        if f64::abs(p) > BIG {
+            p3 *= BIG_INV;
+            p2 *= BIG_INV;
+            q3 *= BIG_INV;
+            q2 *= BIG_INV;
+        }
+        if err <= EPSILON {
+            break;
+        }
+    }
+
+    return 1.0 - (f64::exp(ax) * ans);
+}
+
+/// Chi^2 distribution cumulative distribution function
+fn chi2_cdf(x: f64, dof: i32) -> f64 {
+    if dof < 1 || x < 0.0 {
+        return 0.0;
+    } else if dof == 2 {
+        return 1.0 - f64::exp(-0.5 * x);
+    } else {
+        return 1.0 - rl_gamma((dof as f64) / 2.0, x / 2.0);
+    }
+}
 
 /* Define an recursive lambda function for integration over subintervals */
 fn integrate(
@@ -161,16 +276,94 @@ fn integrate_frequency_table(
 fn chi2_test(
     frequencies: &[Float],
     exp_frequencies: &[Float],
-    theta_res: usize,
-    phi_res: usize,
+    _theta_res: usize,
+    _phi_res: usize,
     sample_count: usize,
+    min_exp_frequency: Float,
+    significance_level: Float,
+    num_tests: usize,
 ) -> Result<(), String> {
-    let mut sum = 0.0;
-    for i in 0..frequencies.len() {
-        let diff = frequencies[i] - exp_frequencies[i];
-        sum += diff * diff / exp_frequencies[i];
+    let mut cells: Vec<(Float, usize)> = exp_frequencies
+        .iter()
+        .enumerate()
+        .map(|(i, &v)| (v, i))
+        .collect();
+    cells.sort_by(|a, b| a.0.partial_cmp(&b.0).unwrap());
+
+    assert!(cells[0].0 <= cells[1].0);
+
+    /* Compute the Chi^2 statistic and pool cells as necessary */
+    let mut pooled_frequencies = 0.0;
+    let mut pooled_exp_frequencies = 0.0;
+    let mut chsq = 0.0;
+    let mut _pooled_cells = 0;
+    let mut dof = 0;
+
+    for c in cells.iter() {
+        if exp_frequencies[c.1] == 0.0 {
+            if frequencies[c.1] > sample_count as Float * 1e-5 {
+                let result = format!(
+                    "Encountered {} samples in a c with expected frequency 0. Rejecting the null hypothesis!",
+                    frequencies[c.1]
+                );
+                return Err(result);
+            }
+        } else if exp_frequencies[c.1] < min_exp_frequency {
+            /* Pool cells with low expected frequencies */
+            pooled_frequencies += frequencies[c.1];
+            pooled_exp_frequencies += exp_frequencies[c.1];
+            _pooled_cells += 1;
+        } else if pooled_exp_frequencies > 0.0 && pooled_exp_frequencies < min_exp_frequency {
+            /*
+            Keep on pooling cells until a sufficiently high
+            expected frequency is achieved.
+            */
+            pooled_frequencies += frequencies[c.1];
+            pooled_exp_frequencies += exp_frequencies[c.1];
+            _pooled_cells += 1;
+        } else {
+            let diff = frequencies[c.1] - exp_frequencies[c.1];
+            chsq += (diff * diff) / exp_frequencies[c.1];
+            dof += 1;
+        }
     }
-    Ok(())
+
+    if pooled_exp_frequencies > 0.0 || pooled_frequencies > 0.0 {
+        let diff = pooled_exp_frequencies - pooled_frequencies;
+        chsq += (diff * diff) / pooled_exp_frequencies;
+        dof += 1;
+    }
+
+    /* All parameters are assumed to be known, so there is no
+    additional DF reduction due to model parameters */
+    dof -= 1;
+
+    if dof <= 0 {
+        let result = format!("The number of degrees of freedom {} is too low!", dof);
+        return Err(result);
+    }
+
+    /* Probability of obtaining a test statistic at least
+    as extreme as the one observed under the assumption
+    that the distributions match */
+    let pval = 1.0 - chi2_cdf(chsq as f64, dof) as Float;
+
+    /* Apply the Sidak correction term, since we'll be conducting multiple
+    independent
+    hypothesis tests. This accounts for the fact that the probability of a
+    failure
+    increases quickly when several hypothesis tests are run in sequence. */
+    let alpha = 1.0 - Float::powf(1.0 - significance_level, 1.0 / num_tests as Float);
+
+    if pval < alpha || !pval.is_finite() {
+        let result = format!(
+            "Rejected the null hypothesis (p-value = {}, significance level = {}",
+            pval, alpha
+        );
+        return Err(result);
+    } else {
+        return Ok(());
+    }
 }
 
 fn test_bsdf(create_bsdf: &(dyn Fn(&mut BSDF, &mut MemoryArena)), description: &str) {
@@ -199,11 +392,11 @@ fn test_bsdf(create_bsdf: &(dyn Fn(&mut BSDF, &mut MemoryArena)), description: &
         }
     }
     if let Some(bsdf) = bsdf.as_ref() {
-        for _ in 0..CHI2_RUNS {
+        for k in 0..CHI2_RUNS {
             /* Randomly pick an outgoing direction on the hemisphere */
             let sample = Point2f::new(rng.uniform_float(), rng.uniform_float());
             let wo_l = cosine_sample_hemisphere(&sample);
-            let wo = bsdf.world_to_local(&wo_l);
+            let wo = bsdf.local_to_world(&wo_l);
             let frequencies =
                 frequency_table(bsdf, &wo, &mut rng, sample_count, theta_res, phi_res);
             //
@@ -215,8 +408,11 @@ fn test_bsdf(create_bsdf: &(dyn Fn(&mut BSDF, &mut MemoryArena)), description: &
                 theta_res,
                 phi_res,
                 sample_count,
+                CHI2_MINFREQ,
+                CHI2_SLEVEL,
+                CHI2_RUNS,
             ) {
-                panic!("{}: {}", description, e);
+                assert!(false, "{}: {}, iteration {}", description, e, k);
             }
         }
     }
@@ -228,7 +424,182 @@ fn create_lambertian(bsdf: &mut BSDF, _arena: &mut MemoryArena) {
     bsdf.add(&r);
 }
 
+fn create_microfacet(
+    bsdf: &mut BSDF,
+    _arena: &mut MemoryArena,
+    beckmann: bool,
+    samplevisible: bool,
+    roughx: Float,
+    roughy: Float,
+) {
+    let ks = Spectrum::from(1.0);
+    //MicrofacetDistribution* distrib;
+    let distrib: Box<dyn MicrofacetDistribution> = if beckmann {
+        let alphax = BeckmannDistribution::roughness_to_alpha(roughx);
+        let alphay = BeckmannDistribution::roughness_to_alpha(roughy);
+        Box::new(BeckmannDistribution::new(alphax, alphay, samplevisible))
+    } else {
+        let alphax = TrowbridgeReitzDistribution::roughness_to_alpha(roughx);
+        let alphay = TrowbridgeReitzDistribution::roughness_to_alpha(roughy);
+        Box::new(TrowbridgeReitzDistribution::new(
+            alphax,
+            alphay,
+            samplevisible,
+        ))
+    };
+    let fresnel = Box::new(FresnelNoOp::new());
+    let bxdf: Arc<dyn BxDF> = Arc::new(MicrofacetReflection::new(&ks, distrib, fresnel));
+    bsdf.add(&bxdf);
+}
+
+fn create_fresnel_blend(
+    bsdf: &mut BSDF,
+    _arena: &mut MemoryArena,
+    beckmann: bool,
+    samplevisible: bool,
+    roughx: Float,
+    roughy: Float,
+) {
+    let d = Spectrum::from(0.5);
+    let s = Spectrum::from(0.5);
+    let distrib: Box<dyn MicrofacetDistribution> = if beckmann {
+        let alphax = BeckmannDistribution::roughness_to_alpha(roughx);
+        let alphay = BeckmannDistribution::roughness_to_alpha(roughy);
+        Box::new(BeckmannDistribution::new(alphax, alphay, samplevisible))
+    } else {
+        let alphax = TrowbridgeReitzDistribution::roughness_to_alpha(roughx);
+        let alphay = TrowbridgeReitzDistribution::roughness_to_alpha(roughy);
+        Box::new(TrowbridgeReitzDistribution::new(
+            alphax,
+            alphay,
+            samplevisible,
+        ))
+    };
+    let bxdf: Arc<dyn BxDF> = Arc::new(FresnelBlend::new(&d, &s, distrib));
+    bsdf.add(&bxdf);
+}
+
 #[test]
 fn bsdf_sampling_lambertian() {
     test_bsdf(&create_lambertian, "Lambertian");
+}
+
+#[test]
+fn bsdf_sampling_beckmann_va_0p5() {
+    test_bsdf(
+        &|bsdf, arena| {
+            create_microfacet(bsdf, arena, true, true, 0.5, 0.5);
+        },
+        "Beckmann, visible area sample, alpha = 0.5",
+    );
+}
+
+#[test]
+fn bsdf_sampling_tr_va_0p5() {
+    test_bsdf(
+        &|bsdf, arena| {
+            create_microfacet(bsdf, arena, false, true, 0.5, 0.5);
+        },
+        "Trowbridge-Reitz, visible area sample, alpha = 0.5",
+    );
+}
+
+#[test]
+fn bsdf_sampling_beckmann_std_0p5() {
+    test_bsdf(
+        &|bsdf, arena| {
+            create_microfacet(bsdf, arena, true, false, 0.5, 0.5);
+        },
+        "Beckmann, std sample, alpha = 0.5",
+    );
+}
+
+#[test]
+fn bsdf_sampling_tr_std_0p5() {
+    test_bsdf(
+        &|bsdf, arena| {
+            create_microfacet(bsdf, arena, false, false, 0.5, 0.5);
+        },
+        "Trowbridge-Reitz, std sample, alpha = 0.5",
+    );
+}
+
+#[test]
+fn bsdf_sampling_beckmann_va_0p2_0p1() {
+    test_bsdf(
+        &|bsdf, arena| {
+            create_microfacet(bsdf, arena, true, true, 0.2, 0.1);
+        },
+        "Beckmann, visible area sample, alpha = 0.2/0.1",
+    );
+}
+
+#[test]
+fn bsdf_sampling_tr_va_0p3_0p15() {
+    test_bsdf(
+        &|bsdf, arena| {
+            create_microfacet(bsdf, arena, false, true, 0.3, 0.15);
+        },
+        "Trowbridge-Reitz, visible area sample, alpha = 0.3/0.15",
+    );
+}
+
+#[test]
+fn bsdf_sampling_beckmann_std_0p2_0p1() {
+    test_bsdf(
+        &|bsdf, arena| {
+            create_microfacet(bsdf, arena, true, false, 0.2, 0.1);
+        },
+        "Beckmann, std sample, alpha = 0.2/0.1",
+    );
+}
+
+#[test]
+fn bsdf_sampling_tr_std_0p2_0p1() {
+    test_bsdf(
+        &|bsdf, arena| {
+            create_microfacet(bsdf, arena, false, false, 0.2, 0.1);
+        },
+        "Trowbridge-Reitz, std sample, alpha = 0.2/0.1",
+    );
+}
+
+#[test]
+fn bsdf_sampling_beckmann_va_0p4_0p3() {
+    test_bsdf(
+        &|bsdf, arena| {
+            create_fresnel_blend(bsdf, arena, true, true, 0.4, 0.3);
+        },
+        "Fresnel blend Beckmann, visible area sample, alpha = 0.4/0.3",
+    );
+}
+
+#[test]
+fn bsdf_sampling_tr_va_0p3() {
+    test_bsdf(
+        &|bsdf, arena| {
+            create_fresnel_blend(bsdf, arena, false, true, 0.3, 0.3);
+        },
+        "Fresnel blend Trowbridge-Reitz, visible area sample, alpha = 0.3",
+    );
+}
+
+#[test]
+fn bsdf_sampling_beckmann_std_0p2() {
+    test_bsdf(
+        &|bsdf, arena| {
+            create_fresnel_blend(bsdf, arena, true, false, 0.2, 0.2);
+        },
+        "Fresnel blend Beckmann, std sample, alpha = 0.2",
+    );
+}
+
+#[test]
+fn bsdf_sampling_tr_std_0p05_0p1() {
+    test_bsdf(
+        &|bsdf, arena| {
+            create_fresnel_blend(bsdf, arena, false, false, 0.05, 0.1);
+        },
+        "Fresnel blend Trowbridge-Reitz, std sample, alpha = 0.05/0.1",
+    );
 }

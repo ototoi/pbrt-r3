4276e4ae9d0a9aae1c66321f326ba7d4ee88455b
Toru Matsuoka <tx.matsuoka@gmail.com>
2025-03-21 11:04:35 +0900

    Remove warn

--- src/core/integrator/mod.rs
+++ src/core/integrator/mod.rs
@@ -1,9 +1,9 @@
 pub mod integrator;
 pub mod mt;
 pub mod sample_lights;
-pub mod sampler;
+pub mod sampler_integrator;
 
 pub use integrator::*;
 pub use mt::*;
 pub use sample_lights::*;
-pub use sampler::*;
+pub use sampler_integrator::*;
